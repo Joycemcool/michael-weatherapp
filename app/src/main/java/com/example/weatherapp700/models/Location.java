e061483a21614a04ef313d90ec7a4f67b202675f
Michael Trumbull <michael.trumbull@nscc.ca>
2023-09-18 16:40:37 -0300

    Adding views and json parsing.

---
+++ app/src/main/java/com/example/weatherapp700/models/Location.java
@@ -0,0 +1,19 @@
+package com.example.weatherapp700.models;
+
+public class Location {
+    private String name;
+    private String region;
+    private String country;
+
+    public Location(String name, String region, String country){
+        this.name = name;
+        this.region = region;
+        this.country = country;
+    }
+
+    public String getName(){ return name; }
+
+    public String getRegion() { return region; }
+
+    public String getCountry() { return country; }
+}
